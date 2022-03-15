6d46337c57117e0e746424209599ac10c5787efe
UsernameCodeRookie <94690535+UsernameCodeRookie@users.noreply.github.com>
2022-03-15 13:39:02 +0800

    Update judge.v
    readme部分改了一下，fail[2:0]的最高位是x，也即fail[2]:x, fail[1]:y, fail[0]:local

--- code/judge.v
+++ code/judge.v
@@ -8,7 +8,7 @@
 // rst_n((negatively effecitve))
 // dout(the direction of each data package :00-NONE 01-X 10-Y 11-LOCAL)
 
-// fail(001-X 010-Y 100-LOCAL)
+// fail(100-X 010-Y 001-LOCAL)
 
 // released:3/15/2022
 // author: hongyang gu
@@ -28,7 +28,7 @@ endmodule
 
 module priority_all(
     input [2:0] fail, 
-    input clk, rst_n,
+    input clk, rst_n, enable,
     output reg [2:0] pri
     );
     wire one_suc;
@@ -39,7 +39,7 @@ module priority_all(
             // reset
             pri <= 3'b000;
         end
-        else begin
+        else if(enable) begin
             pri[2] <= (pri[2] && one_suc) || fail[2];
             pri[1] <= (pri[1] && one_suc) || fail[1];
             pri[0] <= (pri[0] && one_suc) || fail[0];
@@ -62,7 +62,7 @@ module conflict(
 endmodule
 
 module judge(
-    input clk, rst_n,
+    input clk, rst_n, enable,
     input [1:0] dout_x, dout_y, dout_local,
     output [2:0] fail
     );
@@ -79,6 +79,6 @@ module judge(
     priority_cal PCalYZ(pri[1:0], con[1], clk, rst_n, {fail_0[1], fail_1[0]});
     priority_cal PCalXZ({pri[2], pri[0]}, con[0], clk, rst_n, {fail_1[2], fail_0[0]});
     assign fail = fail_0 | fail_1;
-    priority_all PAll(fail, clk, rst_n, pri);
+    priority_all PAll(fail, clk, rst_n, enable, pri);
 
 endmodule
